d45968e5d0ae569c9b828971c15917e32f9dbae9
xuesoso <xuesoso@gmail.com>
2019-09-06 12:32:28 -0700

    updated README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='DensityPlot',
-    version='0.1.4',
+    version='0.1.5',
     author='YSX',
     author_email='xuesoso@gmail.com',
     packages=setuptools.find_packages(),
